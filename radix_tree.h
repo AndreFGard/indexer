662363581d80962fb1d66ce83955e19103ac9fa0
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2024-08-16 15:53:07 -0300

    add proper header file for the radix tree

--- radix_tree.h
+++ radix_tree.h
@@ -17,22 +17,10 @@ struct radnode {
     radnode(const string st) : s(st), occurrences(0) {}
 };
 
+inline char isprefix(string &a, string&b);
+inline int comparer(string &a, string &b);
 
-class radix {
-    public:
-        radnode *head;
-        string st;
 
-        radix();
-        ~radix();
-
-        radnode *insert(radnode *parentp, string &s, int addoccurences);
-        radnode *find(string s);
-        list<string> traverse(radnode &p, string &depth_indent, string &currstring);
-        inline list<string> traverse(radnode &p, string base_string);
-        list<string> find_by_prefix(string prefix);
-
-};
 
 inline void inserthelp(radix &rt, string s);
 inline void test();
