ee085d3dd8f351232eeaeb2100d8cdf778dba5c6
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2024-08-16 16:54:10 -0300

    create the function headers of the radix class

--- radix_tree.h
+++ radix_tree.h
@@ -20,7 +20,21 @@ struct radnode {
 inline char isprefix(string &a, string&b);
 inline int comparer(string &a, string &b);
 
+class radix {
+    public:
+        radnode *head;
+        string st;
 
+        radix();
+        ~radix();
+
+        radnode *insert(radnode *parentp, string &s, int addoccurences);
+        radnode *find(string s);
+        list<string> traverse(radnode &p, string &depth_indent, string &currstring);
+        inline list<string> traverse(radnode &p, string base_string);
+        list<string> find_by_prefix(string prefix);
+
+};
 
 inline void inserthelp(radix &rt, string s);
 inline void test();
